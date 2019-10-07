ea3035d095325cabad07958bb3da8f693b6068cf
Jis <jmintel7@gmail.com>
2019-10-07 21:16:25 +0530

    created database for posts

--- django_trials/blog/views.py
+++ django_trials/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Jis Mathew',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 1 2019'
-    },
-    {
-        'author': 'Jess Sunny',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 3 2019'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
