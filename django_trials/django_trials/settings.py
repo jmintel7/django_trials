49f31a83ec05fac1072a85a65023ff3f98ac32b8
Jis <jmintel7@gmail.com>
2019-10-13 14:12:33 +0530

    fixed security key vulnerability

--- django_trials/django_trials/settings.py
+++ django_trials/django_trials/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+with open(os.path.join(BASE_DIR+'/config.json')) as config_file:
+    config = json.load(config_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$g!i_h%69mge2#^^n0fbdltk&n!qbo6ck-a&t7)&dh5t2gxaim'
+SECRET_KEY = config['SECURITY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
